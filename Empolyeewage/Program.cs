f9e6334d7eb96a3a85714944062f51638bfcc780
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-14 16:10:42 +0530

    [rajaganapthi3076] add uc5

--- Empolyeewage/Program.cs
+++ Empolyeewage/Program.cs
@@ -11,29 +11,36 @@ namespace Empolyeewage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWagecalUC4");
+            Console.WriteLine("Welcome to EmployeeWagecalUC5");
 
-            int emphrs = 0;
-            int empwage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int emphrs = 0, empwage = 0, totaEmplwage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) ;
             {
-                case IS_PART_TIME:
-                    emphrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    emphrs = 8;
-                    break;
-                default:
-                    emphrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        emphrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        emphrs = 8;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+                empwage = emphrs * EMP_RATE_PER_HOUR;
+                totaEmplwage += empwage;
+                Console.WriteLine("Empwage :" + empwage);
+
+
             }
-            empwage = emphrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Empwage :" + empwage);
+            Console.WriteLine(totaEmplwage* NUM_OF_WORKING_DAYS);
+            Console.WriteLine("totalEmpwage :" + empwage);
             Console.ReadLine();
         }
     }
