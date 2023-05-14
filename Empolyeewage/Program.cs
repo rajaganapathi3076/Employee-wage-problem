20a882493aa29e0fb236d747699964dd7c7539b5
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-14 15:59:04 +0530

    [rajaganapthi3076] add uc3

--- Empolyeewage/Program.cs
+++ Empolyeewage/Program.cs
@@ -10,14 +10,19 @@ namespace Empolyeewage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWageUC2");
+            Console.WriteLine("Welcome to EmployeeWagecalUC2");
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int emphrs = 0;
             int empwage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                emphrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 emphrs = 8;
             }
