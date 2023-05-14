7dae005609eecaa15976297e4a237fe497053e48
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-14 16:22:10 +0530

    [rajaganapthi3076] add uc6

--- Empolyeewage/Program.cs
+++ Empolyeewage/Program.cs
@@ -11,16 +11,17 @@ namespace Empolyeewage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HOUR_IN_MONTH = 10;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWagecalUC5");
-
-            int emphrs = 0, empwage = 0, totaEmplwage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) ;
+            Console.WriteLine("Welcome to EmpolyeeWagecalculUC6");
+            int emphrs = 0, totalemphrs = 0, totaWorkindays = 0;
+            while (totalemphrs <= MAX_HOUR_IN_MONTH && totaWorkindays < NUM_OF_WORKING_DAYS)
             {
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
+                totaWorkindays++;
+                Random random1 = new Random();
+                int empCheck = random1.Next(0, 3);
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -32,15 +33,15 @@ namespace Empolyeewage
                     default:
                         emphrs = 0;
                         break;
-                }
-                empwage = emphrs * EMP_RATE_PER_HOUR;
-                totaEmplwage += empwage;
-                Console.WriteLine("Empwage :" + empwage);
 
+                }
+                totalemphrs = +emphrs;
+                Console.WriteLine("Day :" + totaWorkindays + "emphrs :" + emphrs);
 
             }
-            Console.WriteLine(totaEmplwage* NUM_OF_WORKING_DAYS);
-            Console.WriteLine("totalEmpwage :" + empwage);
+            int totalempwage = totalemphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Totalempwage :" + totalempwage);
+            Console.WriteLine("The total Empolyee wage no of days :" +totalempwage * NUM_OF_WORKING_DAYS);
             Console.ReadLine();
         }
     }
