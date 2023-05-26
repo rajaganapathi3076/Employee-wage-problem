7dd04e57142c8aee806cf3d8f787de86b0a2650e
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-26 19:30:17 +0530

    Modified

--- Empolyeewage/Program.cs
+++ Empolyeewage/Program.cs
@@ -8,6 +8,14 @@ namespace Empolyeewage
 {
     internal class Program
     {
+
+
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HOUR_IN_MONTH = 100;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWageUC2");
