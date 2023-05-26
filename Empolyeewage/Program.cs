8c99ee161404cde1c2d3a85e25244d3ebac5ea4c
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-26 19:45:07 +0530

    new one

--- Empolyeewage/Program.cs
+++ Empolyeewage/Program.cs
@@ -11,11 +11,12 @@ namespace Empolyeewage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HOUR_IN_MONTH = 10;
-        static void Main(string[] args)
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HOUR_IN_MONTH = 100;
+
+
+        public static void CheckEmpolyeeWage()
         {
-            Console.WriteLine("Welcome to EmpolyeeWagecalculUC6");
             int emphrs = 0, totalemphrs = 0, totaWorkindays = 0;
             while (totalemphrs <= MAX_HOUR_IN_MONTH && totaWorkindays < NUM_OF_WORKING_DAYS)
             {
@@ -41,8 +42,14 @@ namespace Empolyeewage
             }
             int totalempwage = totalemphrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Totalempwage :" + totalempwage);
-            Console.WriteLine("The total Empolyee wage no of days :" +totalempwage * NUM_OF_WORKING_DAYS);
+            Console.WriteLine("The total Empolyee wage no of days :" + totalempwage * NUM_OF_WORKING_DAYS);
             Console.ReadLine();
         }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmpolyeeWagecalcultionUC7");
+            CheckEmpolyeeWage();
+
+        }   
     }
 }
