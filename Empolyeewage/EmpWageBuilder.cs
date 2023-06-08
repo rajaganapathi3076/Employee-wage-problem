b49860824c5434d4d659b18255f1ed7f78b07bad
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-06-08 15:28:38 +0530

    adding UC9

---
+++ Empolyeewage/EmpWageBuilder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empolyeewage
+{
+    internal class EmpWageBuilder
+    {
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilder(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            totalEmpWage = 0;
+        }
+
+        public void ComputeEmpWage()
+        {
+            // Compute the total wage for the company
+            // using the instance variables
+
+            int empHrs = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHours = 0;
+
+            while (totalEmpHours < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHours += empHrs;
+            }
+
+            totalEmpWage = totalEmpHours * empRatePerHour;
+        }
+
+        public void DisplayEmpWage()
+        {
+            Console.WriteLine("Total wage for company " + company +  " is "  + totalEmpWage);
+        }
+    }
+}
+
--- Empolyeewage/Program.cs
+++ Empolyeewage/Program.cs
@@ -1,70 +1,31 @@
-﻿using System;
+﻿using Empolyeewage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Empolyeewage
+namespace Employeewage
 {
     internal class Program
     {
-         public const int EMP_RATE_PER_HOUR = 20;
-         public const int NUM_OF_WORKING_DAYS = 20;
-         public int MAX_WORKING_HOUR = 100;
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Compute UC8");
-            CheckEmployeeWage("SMT", 24, 140, 40);
-            CheckEmployeeWage("SS steels", 26, 110, 35);
-            CheckEmployeeWage(" TMT", 23, 130, 65);
-            Console.ReadLine();
+            EmpWageBuilder company1 = new EmpWageBuilder("SS PRIVATE LIMITED", 20, 20, 100);
+            company1.ComputeEmpWage();
+            company1.DisplayEmpWage();
 
+            EmpWageBuilder company2 = new EmpWageBuilder("SMT STEELS", 56, 35, 120);
+            company2.ComputeEmpWage();
+            company2.DisplayEmpWage();
 
-        }
-
-
-        public static void CheckEmployeeWage( string company, int maxWorkingDays, int maxWorkingHrs, int empRatePerHrs)
-        {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            int emphrs = 0;
-            int empwage = 0;
-            int totalWage = 0;
-            int day = 1;
-            int totalHrs = 0;
-          
-            Random random = new Random();
-
-            while (day <= maxWorkingDays && totalHrs < maxWorkingHrs)
-            {
-                
-                
-                int randomInput = random.Next(1, 3);
-                switch (randomInput)
-                {
-                    case IS_PART_TIME:
-                        emphrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        emphrs = 8;
-                        break;
-                    default:
-                        emphrs = 0;
-                        break;
-
-                }
-                empwage=empRatePerHrs* emphrs;
-                totalWage += empwage;
-                totalHrs += emphrs;
-                day++;
-                //Console.WriteLine("Employee Wage per day{0}:{1}", day, empwage);
-            }
-            Console.WriteLine("Total Wage:{0} for {1} of {2}days and TotalHrs :{3}  ", totalWage, company, (day - 1), totalHrs);
-
-
+            EmpWageBuilder company3 = new EmpWageBuilder("TATA STEELS", 23, 60, 120);
+            company2.ComputeEmpWage();
+            company2.DisplayEmpWage();
 
         }
-        
+
     }
+        
+    
 }
